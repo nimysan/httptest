db6c0a0521646bb8990d8df362a5171f2c713edb
nimysan <234aini@gmail.com>
2024-01-29 12:39:34 +0800

    1000000

--- start_worker.sh
+++ start_worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo cat hosts >> /etc/hosts
+cat hosts  | sudo tee -a /etc/hosts
 pip install locust
 nohup locust -f simple.py --worker --master-host=172.31.46.167 --processes -1 > /tmp/locust.out 2>&1 &
